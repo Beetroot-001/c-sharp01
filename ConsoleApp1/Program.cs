af88b5f53718c5ae6035ad16663e10479cce3347
Serhii <sergkropotov@ukr.net>
2022-09-06 11:40:24 +0300

    Extra

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,11 +19,18 @@ namespace ConsoleApp1
             double y = 6;
 
             Console.WriteLine(calc1(x));
+            Console.WriteLine("/////*** 1/////");
             Console.WriteLine(calc2(x));
+            Console.WriteLine("/////*** 2/////");
             Console.WriteLine(calc3(x));
+            Console.WriteLine("/////*** 3/////");
             Console.WriteLine(calc4(x,y));
+            Console.WriteLine("/////*** 4/////");
             Console.WriteLine(leftToNY());
-            Console.WriteLine(leftFromNY());
+            Console.WriteLine("/////*** 5/////");
+            Console.WriteLine(leftFromNY());            
+            Console.WriteLine("/////Extra/////");
+            Console.WriteLine(leftToNY2("01.01.2022", "12.05.2022"));
 		}
 
         static double calc1(double x) => -6 * Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 10 * x + 15;
@@ -32,5 +39,22 @@ namespace ConsoleApp1
         static double calc4(double x, double y) => Math.Max(x,y);
         static int leftToNY() => DateTime.Now.DayOfYear;
         static int leftFromNY() => DateTime.IsLeapYear(DateTime.Now.Year) ? 366 : 365 - leftToNY();
+        static int leftToNY2(string startDate, string endDate)
+        {
+            var sDate = DateTime.Parse(startDate);
+            var eDate = DateTime.Parse(endDate);
+            var mondaysCount = 0;
+            while(sDate <= eDate)
+            {
+                if((int)sDate.DayOfWeek == 1)
+                {
+                    mondaysCount++;
+                }
+                sDate = sDate.AddDays(1);
+            }
+
+            return mondaysCount;
+        }
+
     }
 }
