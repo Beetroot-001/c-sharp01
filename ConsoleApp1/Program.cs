59da6cf7c2405b4a056ff8f0e4da12f0531c2d2e
Vladyslav <qa.balitskyi@gmail.com>
2022-09-09 17:07:03 +0300

    Homework_3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,43 @@
 	{
 		static void Main(string[] args)
 		{
-		}
-	}
+
+            int[,] array2d = new int[,] 
+            { 
+                { 10, 5, 10 }, 
+                { 12, 2, 10 }
+            };
+
+            for (int element = 0; element < 3; element++)
+            {
+                int x = (array2d[0, element]);
+                int y = (array2d[1, element]);
+
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+                Console.WriteLine("---");
+
+                List<int> list = new List<int>();
+
+                for (int s = Math.Min(x,y); s <= Math.Max(x, y); s++)
+                {
+                    list.Add(s);
+                    continue;
+                }
+                list.ForEach(Console.WriteLine);
+                Console.WriteLine("--------");
+                Console.WriteLine("sum of all values: " + list.Sum());
+                Console.WriteLine("-------------------------------------");
+            }
+
+            int c = 7;
+            int t = 5;
+            Console.WriteLine(c);
+            Console.WriteLine(t);
+
+            (c, t) = (t, c);
+            Console.WriteLine(c);
+            Console.WriteLine(t);
+        }
+    }
 }
