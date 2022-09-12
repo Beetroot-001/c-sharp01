029ca9d3f6515cb7c462906a92bb7df5d6510a15
SerezhkaGit <sergiy.kusiv@gmail.com>
2022-09-13 02:05:56 +0300

    change 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,42 @@
-﻿namespace ConsoleApp1
+﻿using System;
+
+public static class StringConversion
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-		
+    public static void Main()
+    {
+       
+        //homework task
 
+        for (; ; )
+        {
+            Console.WriteLine("\nEnter the value of x");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y");
+            int y = Convert.ToInt32(Console.ReadLine());
+            
+            { 
+            int sum = x + y;
+                if (x == y)
+                {
+                    Console.WriteLine($"The Sum = {x}\n>>To left Enter L\n >>To continue PRESS --enter--");
+                    if (Console.ReadLine() == "L")
+                    {
+                        break;
+                    }
 
+                }
+                else
+                {
+                    Console.WriteLine($"The Sum = {sum=x+y}\n>>To left Enter L\n>>To continue PRESS --enter--");
+                    if (Console.ReadLine() == "L")
+                    {
+                        break;
+                    }
+                }
 
-
-
-
+            }
         }
+       
+
     }
 }
