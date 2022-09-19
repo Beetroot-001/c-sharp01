6e0df5fa8c0cd8fa420d32d5cb6c9d9c67b59d7a
an-lev <lewanatolij@gmail.com>
2022-09-19 18:42:23 +0300

    add game

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,44 @@
 	{
 		static void Main(string[] args)
 		{
+			var students = new[]
+			{
+				"Sergiy Kuziv",
+				"Oleh Slobodeniuk",
+				"Serhii Kropotov",
+				"Popov Artem",
+				"Maryna Bohdanova",
+				"Balitskyi Vladyslav",
+				"Andrii Kovalenko",
+				"Ihor Kovalchuk",
+				"Ponomarienko Andrii",
+				"Nataliia Tyshchenko",
+				"Rushynets Oleksii",
+				"Viktoriya Tsaruk",
+				"Bas Serhii",
+			};
+
+			Console.WriteLine("\tThe game is started!");
+			var randomizer = new Random((int)DateTime.Now.Ticks);
+
+			while (true)
+			{
+				Console.WriteLine("Press 0 to exit, in another case - continue");
+				var consoleKey = Console.ReadKey();
+				if (consoleKey.Key == ConsoleKey.D0) Environment.Exit(0);
+
+				Console.Clear();
+				Console.WriteLine("...");
+				Thread.Sleep(300);
+				Console.Clear();
+				Console.WriteLine("...");
+				Thread.Sleep(300);
+				Console.Clear();
+				Console.WriteLine("...");
+				Thread.Sleep(300);
+				Console.Clear();
+				Console.WriteLine($"Answering: {students[randomizer.Next(0, students.Length)]}");
+			}
 		}
 	}
 }
