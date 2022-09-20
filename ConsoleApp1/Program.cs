9bbc3b10b1909d3b1abbb4dfe3b9e9cca390e5d0
an-lev <lewanatolij@gmail.com>
2022-09-20 20:22:10 +0300

    improve the game

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,8 +2,12 @@
 {
 	internal class Program
 	{
+		public const string Exceptions = "exceptions.txt";
+
 		static void Main(string[] args)
 		{
+			Console.OutputEncoding = System.Text.Encoding.Unicode;
+
 			var students = new[]
 			{
 				"Sergiy Kuziv",
@@ -20,27 +24,47 @@
 				"Viktoriya Tsaruk",
 				"Bas Serhii",
 			};
+			var answerCount = new int[students.Length];
 
 			Console.WriteLine("\tThe game is started!");
 			var randomizer = new Random((int)DateTime.Now.Ticks);
 
-			while (true)
+			var questions = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), Exceptions));
+
+			foreach (var question in questions)
 			{
+				Console.WriteLine("-------------------");
+				Console.WriteLine(question);
+
+				Console.WriteLine("...");
+				Thread.Sleep(300);
+				Console.WriteLine("...");
+				Thread.Sleep(300);
+				Console.WriteLine("...");
+				Thread.Sleep(300);
+
+				var random = randomizer.Next(0, students.Length);
+				var student = students[random];
+
+				Console.WriteLine($"Answering: {student}");
+				answerCount[random]++;
+
+				if (answerCount[random] > 2)
+				{
+					students = students.Where(x => x != student).ToArray();
+				}
+
+				if (students.Length == 0)
+				{
+					Console.WriteLine("All students are done");
+					Environment.Exit(0);
+				}
+
 				Console.WriteLine("Press 0 to exit, in another case - continue");
 				var consoleKey = Console.ReadKey();
 				if (consoleKey.Key == ConsoleKey.D0) Environment.Exit(0);
 
 				Console.Clear();
-				Console.WriteLine("...");
-				Thread.Sleep(300);
-				Console.Clear();
-				Console.WriteLine("...");
-				Thread.Sleep(300);
-				Console.Clear();
-				Console.WriteLine("...");
-				Thread.Sleep(300);
-				Console.Clear();
-				Console.WriteLine($"Answering: {students[randomizer.Next(0, students.Length)]}");
 			}
 		}
 	}
