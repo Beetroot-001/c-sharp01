be7df744c976412a0869a948e4600acd34674515
github-entity <bogdanushka@gmail.com>
2022-09-26 00:03:54 +0300

    Library : TODO - need to add UML

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp1
+
+{
+    internal class Book
+    {
+        public Author AuthorOfBook { get; set; }
+        public string BookName { get; set; }    
+        public string Description { get; set; }
+        public string[] Chapter { get; set; }
+        public int TotalPages { get; set; }
+        public bool Pictures { get; set; }
+        public bool IsInTheLibraryNow { get; set; }
+        public static  int HowManyCopiesInTotal { get; set; }
+        public DateTime WhenNeedToBeReturnedToTheLibrary { get; set; }
+        public static bool IsFoud { get; set; }
+
+        public Book(Author authorOfBook, string bookName, string description, string[] chapter, int totalPages, bool pictures, DateTime whenNeedToBeReturnedToTheLibrary, bool iSInTheLibraryNow = false, int howManyCopiesInTotal = 0)
+        {
+            AuthorOfBook = authorOfBook;
+            BookName = bookName;
+            Description = description;
+            Chapter = chapter;
+            TotalPages = totalPages;
+            Pictures = pictures;
+            IsInTheLibraryNow = iSInTheLibraryNow;
+            HowManyCopiesInTotal = howManyCopiesInTotal;
+            WhenNeedToBeReturnedToTheLibrary = whenNeedToBeReturnedToTheLibrary;
+        }
+
+
+        public  void ReedBook()
+        {
+
+        }
+
+        public static bool FindBook(Book bookName, Author firstName, Author lastName)
+        {
+            return IsFoud;
+;       }
+    }
+}
---
+++ ConsoleApp1/Library.cs
@@ -0,0 +1,55 @@
+﻿namespace ConsoleApp1
+{
+    internal class Library
+    {
+        public Book Books { get; set; }
+        public int TotalAmountOfBooksInLibrary { get; set; }
+        public Reader Reader { get; set; }
+        public int TotalReaderCardsAmount { get; set; }
+        public ReaderCard[] ReaderCardsDataBase { get; set; }
+        
+        //FindBook
+        public bool FindBook(Book bookName, Author firstName, Author lastName)
+        {
+            return Book.FindBook(bookName, firstName, lastName);
+        }
+        
+        //Add book to the library
+        public int AddBookToTheLibrary(Book bookName, int HowManyCopiesInTotal)
+        {
+            bookName.IsInTheLibraryNow = true;
+            return TotalAmountOfBooksInLibrary += HowManyCopiesInTotal;
+        }
+
+        //Delete book from the library
+        public int DeleteBookFromLibrary(Book bookName, int howManyCopiesOfSpecificBookInTotal)
+        {
+            bookName.IsInTheLibraryNow = false;
+            return  TotalAmountOfBooksInLibrary -= howManyCopiesOfSpecificBookInTotal;
+        }
+
+
+        // Add or delete reader card
+        public ReaderCard[] AddDeleteReaderCard(ReaderCard[] ReaderCardsDataBase, ReaderCard card, bool addTrueDeleteFalse)
+        {
+            var cardDataBase = ReaderCardsDataBase.ToList();
+            for (int i = 0; i < cardDataBase.Count; i++)
+            {
+                if (cardDataBase[i].CardNumber == card.CardNumber && addTrueDeleteFalse == false)
+                {
+                    cardDataBase.Remove(cardDataBase[i]);
+                }
+                else if (cardDataBase[i].CardNumber != card.CardNumber && addTrueDeleteFalse == true)
+                {
+                    cardDataBase.Add(cardDataBase[i]);
+                }
+                else if (cardDataBase[i].CardNumber != card.CardNumber && addTrueDeleteFalse == true)
+                {
+                    Console.WriteLine("Can't find this card");
+                }
+            }
+
+            return cardDataBase.ToArray();
+        }
+    }
+}
