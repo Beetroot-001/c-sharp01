4d6fa22d33eb4e009884fc8b078bf6a881a66772
Anatolii Levchenko <Anatolii.Levchenko@outlook.com>
2022-09-30 21:00:59 +0300

    classwork

---
+++ ClassLibrary2/Animal.cs
@@ -0,0 +1,61 @@
+﻿namespace ClassLibrary2
+{
+
+	public enum EyeColor
+	{
+		Blue,
+		Grey,
+		Green,
+		Yellow
+	}
+
+	public enum EatingStyle
+	{
+		Meat,
+		Grass,
+		Omnivore
+	}
+
+	public enum Mood
+	{
+		Sad,
+		Happy,
+		Neutral
+
+	}
+
+	public class Animal
+	{
+		public static int MyProperty { get; set; }
+		protected EyeColor EyeColor { get; set; }
+		public EatingStyle EatingStyle { get; set; }
+		public bool IsAlive { get; set; }
+		public Mood Mood { get; set; }
+
+		public Animal(EyeColor eyeColor)
+		{
+			EyeColor = eyeColor;
+			Console.WriteLine("CTR Animal");
+		}
+
+		public virtual int Say()
+		{
+			Console.WriteLine("Animal Say");
+			return 0;
+		}
+	}
+
+	public class Dog : Animal
+	{
+		public Dog(EyeColor eyeColor) : base(eyeColor)
+		{
+		}
+
+		public new int Say()
+		{
+			Console.WriteLine("DOG Say: Bark");
+
+			return 0;
+		}
+	}
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,120 @@
-﻿namespace ConsoleApp1
+﻿using ClassLibrary2;
+
+namespace ConsoleApp1
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
+
+			//var animal = new Animal();
+
+			//var dog = new Dog();
+
+			Cat cat = new Cat(EyeColor.Green);
+			Animal animalCat = new Cat(EyeColor.Blue);
+			Animal animalDog = new Dog(EyeColor.Blue);
+
+			cat.Say();
+			animalCat.Say();
+			animalDog.Say();
+
+			bool isDog = animalDog is Dog;
+			Dog anotherDog = (Dog)animalCat;
+
+			anotherDog.Say();
+
+			Console.WriteLine(cat);
+
+			Console.WriteLine("Hash codes");
+
+			var cat1 = new Cat(EyeColor.Grey)
+			{
+				Breed = Breed.Siamese,
+				EatingStyle = EatingStyle.Omnivore,
+				Mood = Mood.Happy
+			};
+
+			var cat2 = new Cat(EyeColor.Grey)
+			{
+				Breed = Breed.Siamese,
+				EatingStyle = EatingStyle.Omnivore,
+				Mood = Mood.Happy
+			};
+
+			var cat3 = new Cat(EyeColor.Blue)
+			{
+				Breed = Breed.Siamese,
+				EatingStyle = EatingStyle.Grass,
+				Mood = Mood.Neutral
+			};
+
+			Console.WriteLine($"{cat1.GetHashCode()} {cat2.GetHashCode()}");
+			Console.WriteLine($"{cat1.GetHashCode()} {cat3.GetHashCode()}");
+			Console.WriteLine(cat1.Equals(cat2));
+			Console.WriteLine(cat1.Equals(cat3));
+			Console.WriteLine(cat2.Equals(cat3));
+			Console.WriteLine(cat2.Equals(new Dog(EyeColor.Yellow)));
+		}
+	}
+
+	public enum Breed
+	{
+		Siamese,
+		Asian,
+		Ashera
+	}
+
+
+	public class Cat : Animal
+	{
+		public Breed Breed { get; set; }
+
+		public Cat(EyeColor eyeColor) : base(eyeColor)
+		{
+			Console.WriteLine("CTR Cat");
+		}
+
+		public override int Say()
+		{
+			Console.WriteLine("Cat SAY");
+			return 0;
+		}
+
+		public void Move()
+		{
+			throw new NotImplementedException();
+		}
+
+
+		public override string ToString()
+		{
+			return $"Breed {Breed} EyeColor {EyeColor}";
+		}
+
+		public override int GetHashCode()
+		{
+			return 17 * (int)Breed ^ (int)EyeColor ^ (int)Mood ^ (int)EatingStyle;
+		}
+
+		public override bool Equals(object? obj)
+		{
+			// check null
+
+			if (obj == null)
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, obj))
+			{
+				return true;
+			}
+
+			Cat other = obj as Cat;
+			if (other == null) return false;
+
+			return other.EyeColor == EyeColor && other.IsAlive == IsAlive && other.Breed == Breed;
 		}
 	}
 }
