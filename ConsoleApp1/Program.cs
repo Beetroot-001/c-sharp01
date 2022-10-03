c3f8a5fae79ba9062a6611ae89b387f98dd16cbc
Tysyatsky <tysyatsky@gmail.com>
2022-10-03 14:10:58 +0300

    Branch upload
    Branch upload with classes core
    1. Vehicle
    2. Worker
    3. VehicleSystem

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,111 @@
 	{
 		static void Main(string[] args)
 		{
+			Mechanic m = new Mechanic("John", "John", "", 18);
+			Console.WriteLine(m.Name);
 		}
 	}
+
+	class Vehicle
+	{
+		private int _wheelCount;
+
+	} 
+
+	class Car : Vehicle
+	{
+
+	}
+
+	class Bicycle : Vehicle
+	{
+
+	}
+
+	class Truck : Vehicle
+	{
+
+	}
+
+	public enum VehicleStateS
+    {
+		New,
+		Working,
+		Broken,
+		Repeared
+	}
+
+	class VehicleSystem
+	{
+		private string _name;
+		protected VehicleStateS _state;
+
+		public string Name => _name;
+
+		public VehicleStateS State => _state;
+
+		public VehicleSystem(string name)
+		{
+			_name = name;
+			_state = VehicleStateS.New;
+		}
+		public VehicleSystem(string name, VehicleStateS state) : this (name)
+		{
+			_name = name;
+			_state = state;
+		}
+
+		public virtual void Break()
+		{
+            _state = VehicleStateS.Broken;
+			Console.WriteLine("Vehicle system broken successfully");
+        } 
+	}
+
+	class MovingSystem : VehicleSystem
+	{
+		private bool _isWheesMoving;
+		private bool _isBouncing;
+		public MovingSystem(string name, VehicleStateS state) : base(name, state)
+		{
+			_isBouncing = true;
+			_isWheesMoving = true;
+		}
+
+        public bool IsBroken => !(_isBouncing && _isWheesMoving);
+        public override void Break()
+		{
+            _isWheesMoving = _isBouncing = false;
+			_state = VehicleStateS.Broken;
+            Console.WriteLine("Moving system broken successfully");
+        }
+		
+	}
+
+	class CoolingSystem : VehicleSystem
+	{
+		private bool _isCooling;
+        public bool IsBroken => !_isCooling;
+
+        public CoolingSystem(string name, VehicleStateS state) : base(name, state)
+		{
+			_isCooling = true;
+		}
+
+		public override void Break()
+		{
+			_isCooling = false;
+			_state = VehicleStateS.Broken;
+            Console.WriteLine("Colling system broken successfully");
+		}
+	}
+
+	class BreakSystem : VehicleSystem
+	{	
+
+		public BreakSystem(string name, VehicleStateS state) : base(name, state)
+		{
+		}
+	}
+
 }
