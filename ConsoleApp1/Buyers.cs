72f99d0866549425b18a682f3871b5d8ba7cfb73
github-entity <bogdanushka@gmail.com>
2022-10-03 21:44:31 +0300

    Internet shop

---
+++ ConsoleApp1/Buyers.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Buyers
+    {
+        public List<string> PersonalInformation { get; set; }
+        public DiscountCardOwner DiscountCardOwner { get; set; }
+        public List<string> BoughtFoodList { get; set; }
+        public DateTime ordersDay;
+
+        public Buyers(List<string> personalInformation, DiscountCardOwner discountCardOwner, List<string> boughtFoodList, DateTime ordersDate)
+        {
+            PersonalInformation = personalInformation;
+            DiscountCardOwner = discountCardOwner;
+            BoughtFoodList = boughtFoodList;
+            ordersDay = ordersDate;
+        }
+        
+    }
+
+    public enum DiscountCardOwner
+    {
+        Yes,
+        No
+    }
+}
---
+++ ConsoleApp1/Products.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Products
+    {
+        public int price;
+        public int quantity;
+        public Discount Discount { get; set; }
+        string productName;
+        int totalAvailable;
+
+        public Products(int price, int quantity, string productName, int totalAvailable)
+        {
+            this.price = price;
+            this.quantity = quantity;
+            this.productName = productName; 
+            this.totalAvailable = totalAvailable;   
+        }
+
+    }
+
+    public enum Discount
+    {
+        InDiscount,
+        ExcludedFromDiscount
+    }
+}
