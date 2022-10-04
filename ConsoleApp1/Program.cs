522c83c6baaa6ab4dac0ce10dd4b19e68ac7999d
github-entity <bogdanushka@gmail.com>
2022-10-04 17:29:58 +0300

    Checkin with updates

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,12 @@
 	{
 		static void Main(string[] args)
 		{
-		}
+			Products apple = new Products(5, 6, "Apple", 10);
+			IShop shop = new InternetShop();
+
+			shop.RegisterNewProduct(apple);
+			shop.SellProduct(apple);
+
+        }
 	}
 }
