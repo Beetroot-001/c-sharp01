6890cb4789620a9114737ef9134e7982f06f22db
Serhii <sergkropotov@ukr.net>
2022-10-05 11:43:24 +0300

    lesson 14. hw  - gotdone

---
+++ ConsoleApp1/Buyer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Buyer
+    {
+        private string _fname;
+        private string FName
+        {
+            get => _fname;
+            set => _fname = value;
+        }
+
+        private string _lname;
+        private string LName
+        {
+            get => _lname;
+            set => _lname = value;
+        }
+
+        private string _mname;
+        private string MName
+        {
+            get => _mname;
+            set => _mname = value;
+        }
+
+        private string _phone;
+        private string Phone
+        {
+            get => _phone;
+            set => _phone = value;
+        }
+    }
+}
---
+++ ConsoleApp1/Receipt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Receipt
+    {
+        public Buyer Buyer { get; private set; }
+        public List<Product> Producs { get; set; } = new List<Product>();
+        
+        public Receipt(Buyer buyer)
+        {
+            Buyer = buyer;
+        }
+    }
+}
