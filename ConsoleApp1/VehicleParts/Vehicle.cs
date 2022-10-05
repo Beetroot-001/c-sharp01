29e73f297ea505b44349eca93deca5d3a56a36cb
Serhii <sergkropotov@ukr.net>
2022-10-05 12:55:01 +0300

    upd. Add code

---
+++ ConsoleApp1/VehicleParts/Vehicle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.VehicleParts
+{
+    internal class Vehicle
+    {
+        public enum VehicleType
+        {
+            Auto,
+            Motorcycle
+        }
+        
+        public enum VehicleStatus
+        {
+            Arrived,
+            InRepairing,
+            GotRepaired,
+            Issued
+        }
+
+        public string Id { get; set; }
+
+        public VehicleType Type { get; set; }
+
+        public VehicleStatus Status { get; set; }
+
+        public Engine Engine { get; set; }
+        public List<Wheel> Wheels { get; set; }
+
+        public void ChangeStatus(VehicleStatus vehicleStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncludeMainWorker(Worker worker)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleApp1/VehicleParts/Wheel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.VehicleParts
+{
+    internal class Wheel
+    {
+        public float Diameter { get; set; }
+    }
+}
