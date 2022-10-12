339b0201c7a49b90d25f3272d7226efd7c3048d0
Anatolii Levchenko <Anatolii.Levchenko@outlook.com>
2022-10-12 21:06:59 +0300

    classwork

---
+++ ConsoleApp1/LinkedList.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	public class ListElement<T>
+	{
+		public T Data { get; set; }
+		public ListElement<T> Next { get; set; }
+	}
+
+	public class LinkedList<T>
+	{
+		private ListElement<T> _head;
+
+		public int Length { get; private set; }
+		public void Add(T value)
+		{
+			ListElement<T> newElement = new ListElement<T>
+			{
+				Data = value
+			};
+
+			if (_head == null)
+			{
+				_head = newElement;
+			}
+			else
+			{
+				var currentElement = _head;
+				while (currentElement.Next != null)
+				{
+					currentElement = currentElement.Next;
+				}
+
+				currentElement.Next = newElement;
+			}
+
+			Length++;
+		}
+
+
+		public T Remove(int position)
+		{
+			if (position < 0 || position > Length - 1)
+			{
+				throw new ArgumentOutOfRangeException(nameof(position));
+			}
+
+			ListElement<T> elementToRemove = null;
+
+			if (position == 0)
+			{
+				elementToRemove = _head;
+				_head = _head.Next;
+			}
+			else
+			{
+				ListElement<T> prevElement = null;
+				elementToRemove = _head;
+				for (int i = 0; i < position; i++)
+				{
+					prevElement = elementToRemove;
+					elementToRemove = elementToRemove.Next;
+				}
+
+				prevElement.Next = elementToRemove.Next;
+			}
+
+			Length--;
+			return elementToRemove.Data;
+		}
+
+		public void Insert(int position, T data)
+		{
+			if (position < 0 || position > Length)
+			{
+				throw new ArgumentOutOfRangeException(nameof(position));
+			}
+
+			var newElement = new ListElement<T>()
+			{
+				Data = data
+			};
+
+			if (position == 0)
+			{
+				newElement.Next = _head;
+				_head = newElement;
+			}
+			else
+			{
+				ListElement<T> prevElement = null;
+				var currentElement = _head;
+
+				for (int i = 0; i < position; i++)
+				{
+					prevElement = currentElement;
+					currentElement = currentElement.Next;
+				}
+
+				prevElement.Next = newElement;
+				newElement.Next = currentElement;
+			}
+
+			Length++;
+		}
+
+		public T GetByIndex(int position)
+		{
+			if (position < 0 || position > Length - 1)
+			{
+				throw new ArgumentOutOfRangeException(nameof(position));
+			}
+
+			var currentElement = _head;
+
+			for (int i = 0; i < position; i++)
+			{
+				currentElement = currentElement.Next;
+			}
+
+			return currentElement.Data;
+		}
+
+		public T this[int index]
+		{
+			get => GetByIndex(index);
+			set => Insert(index, value);
+		}
+
+		public void CopyTo(T[] values)
+		{
+			var currentElement = _head;
+			for (int i = 0; i < values.Length && currentElement != null; i++)
+			{
+				values[i] = currentElement.Data;
+				currentElement = currentElement.Next;
+			}
+		}
+
+		public T[] ToArray()
+		{
+			T[] values = new T[Length];
+			CopyTo(values);
+
+			return values;
+		}
+	}
+
+	public class Person
+	{
+		public string Fullname { get; set; }
+	}
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,138 @@
 	{
 		static void Main(string[] args)
 		{
+			int a = 10;
+			int b = 20;
+			Display(a, b);
+			Swap(ref a, ref b);
+			Display(a, b);
+
+
+			string s1 = "hello";
+			string s2 = "world";
+			Display(s1, s2);
+			Swap(ref s1, ref s2);
+			Display(s1, s2);
+
+			Display<object>(a, s1);
+
+			LinkedList<Person> linkedList = new LinkedList<Person>();
+
+			Console.WriteLine($"List length: {linkedList.Length}");
+			linkedList.Add(new Person() { Fullname = "Petro Gudzyk" });
+			Console.WriteLine($"List length: {linkedList.Length}");
+			linkedList.Add(new Person() { Fullname = "Kyryl Lisoviy" });
+			Console.WriteLine($"List length: {linkedList.Length}");
+			linkedList.Add(new Person() { Fullname = "Taras Shevchenko" });
+			Console.WriteLine($"List length: {linkedList.Length}");
+
+
+			// var removedElement1 = linkedList.Remove(1);
+			// var removedElement2 = linkedList.Remove(1);
+			// var removedElement3 = linkedList.Remove(0);
+			Console.WriteLine($"List length: {linkedList.Length}");
+
+
+			var intLinkedList = new LinkedList<int>();
+			intLinkedList.Add(1);
+			intLinkedList.Add(2);
+			intLinkedList.Add(3);
+
+			intLinkedList.Insert(0, 0);
+			intLinkedList.Insert(2, 2);
+			intLinkedList.Insert(intLinkedList.Length, intLinkedList.Length);
+
+			Console.WriteLine(intLinkedList.GetByIndex(0));
+			Console.WriteLine(intLinkedList.GetByIndex(intLinkedList.Length - 1));
+
+			Console.WriteLine(intLinkedList[0]);
+			Console.WriteLine(intLinkedList[intLinkedList.Length - 1]);
+
+			intLinkedList[0] = 5;
+
+
+			int[] ints = new int[intLinkedList.Length];
+			intLinkedList.CopyTo(ints);
+
+			int[] ints2 = new int[2];
+			intLinkedList.CopyTo(ints2);
+
+			int[] ints3 = new int[20];
+			intLinkedList.CopyTo(ints3);
+
+			int[] ints1 = intLinkedList.ToArray();
+		}
+
+		public static void CoAndContrVariance()
+		{
+			Base base1 = new Base();
+			Base derived1 = new Derived();
+			Derived derived2 = new Derived();
+			// Error: Derived derived3 = new Base();
+
+			base1.DoSomething();
+			derived2.DoSomething();
+			derived2.DoMore();
+
+			IProducer<Base> producerBase = null;
+			Base b1 = producerBase.Produce();
+			// Error: Derived b2 = producerBase.Produce();
+
+			IProducer<Derived> producerDerived = null;
+			Derived d1 = producerDerived.Produce();
+			Base d2 = producerDerived.Produce();
+
+			IProducer<Base> prodOfBase = producerDerived;
+			// Error: IProducer<Derived> prodOfDerived = producerBase;
+
+			IConsumer<Base> consumerB = null;
+			consumerB.Consume(new Base());
+			consumerB.Consume(new Derived());
+
+			IConsumer<Derived> consumerD = null;
+			consumerD.Consume(new Derived());
+			// Error: consumerD.Consume(new Base());
+
+			IConsumer<Derived> consOfDerived = consumerB;
+			// Error: IConsumer<Base> consOfBase = consumerD;
+		}
+
+		public static void Swap<T>(ref T a, ref T b)
+		{
+			T temp = a;
+			a = b;
+			b = temp;
+		}
+
+		public static void Display<T>(T a, T b)
+		{
+			Console.WriteLine($"A: {a.ToString()}, B: {b.ToString()}");
+		}
+
+		public class Base
+		{
+			public void DoSomething()
+			{
+				Console.WriteLine($"Do something {this.GetType()}");
+			}
+		}
+
+		public class Derived : Base
+		{
+			public void DoMore()
+			{
+				Console.WriteLine("Do more");
+			}
+		}
+
+		public interface IProducer<out T>
+		{
+			T Produce();
+		}
+
+		public interface IConsumer<in T>
+		{
+			void Consume(T obj);
 		}
 	}
 }
