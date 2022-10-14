c2791e7e32f7cdef3e2cc020eda9df584040a2a6
SerhiiBas <basserhii@gmail.com>
2022-10-14 13:54:26 +0300

    sbas/homework/generic

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,21 @@
 	{
 		static void Main(string[] args)
 		{
+			Stack<object> stack = new Stack<object>();
+			stack.Push("First");
+			stack.Push(2);
+			stack.Push("third");
+			stack.Push('4');
+            Element<object> dellElement = stack.Pop();
+			stack.Clear();
+			stack.Push("New Element");
+			stack.Push("Serhii");
+			var element= stack.Peek();
+
+			Element<object>[] elements = new Element<object>[7];
+			stack.CopyTo(elements);
+
+			Console.ReadLine();
 		}
 	}
 }
---
+++ ConsoleApp1/Stack.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Stack<T>
+    {
+        private Element<T> _head = new Element<T>();
+
+        /// <summary>
+        /// Property returns number of elements
+        /// </summary>
+        public int Lenth { get; private set; }
+
+        /// <summary>
+        /// Adds obj at the top of the stack
+        /// </summary>
+        /// <param name="value"></param>
+        public void Push(T value)
+        {
+            Element<T> newElement = new Element<T>() { Data = value };
+            if (_head.Next == null)
+            {
+                _head.Next = newElement;
+            }
+            else
+            {
+                newElement.Next = _head.Next;
+                _head.Next = newElement;
+            }
+            Lenth++;
+        }
+
+        /// <summary>
+        /// Returns top element of stack & removes it
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NullReferenceException"></exception>
+        public Element<T> Pop()
+        {
+            if (_head.Next == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            Element<T> currentElement;
+            currentElement = _head;
+            _head = currentElement.Next;
+            Lenth--;
+
+            return currentElement;
+        }
+
+        /// <summary>
+        /// Clear stack
+        /// </summary>
+        public void Clear()
+        {
+            while (_head.Next != null)
+            {
+                Pop();
+            }
+            Console.WriteLine("Stack is Clear");
+        }
+
+        /// <summary>
+        /// Returns top element but doesn't remove it
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NullReferenceException"></exception>
+        public Element<T> Peek()
+        {
+            if (_head.Next==null)
+            {
+                throw new NullReferenceException();
+            }
+            return _head.Next;
+        }
+
+        /// <summary>
+        /// Copies stack to array
+        /// </summary>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        public Element<T>[] CopyTo(Element<T>[] elements)
+        {
+            if (elements.Length<0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            Element<T> element = null;
+            Element<T> currentElement = _head;
+            for (int i = 0; i < elements.Length && i != this.Lenth; i++)
+            {
+                elements[i] = currentElement;
+                element = currentElement;
+                currentElement = _head.Next;
+            }
+            return elements;
+        }
+    }
+}
