dbaf976de345c623c55bc1515d75814d1f0a11c4
AndriiKovalenko90 <108003741+AndriiKovalenko90@users.noreply.github.com>
2022-10-14 16:56:05 +0300

    Only Stack

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,32 @@
 	{
 		static void Main(string[] args)
 		{
+			MyStack<Person> stack = new MyStack<Person>();
+			stack.Push(new Person() { Name = "Andy" });
+			Console.WriteLine(stack.Peek().Name);
+			stack.Push(new Person() { Name = "Bob" });
+			Console.WriteLine(stack.Peek().Name);
+			stack.Push(new Person() { Name = "Carl" });
+			Console.WriteLine(stack.Peek().Name);
+			stack.Push(new Person() { Name = "Dwayne" });
+			Console.WriteLine(stack.Peek().Name);
+			stack.Push(new Person() { Name = "Evan" });
+			Console.WriteLine(stack.Peek().Name);
+			stack.Push(new Person() { Name = "Fred" });
+			Console.WriteLine(stack.Peek().Name);
+			Console.WriteLine(stack.Pop().Name);
+			Console.WriteLine(stack.Peek().Name);
+
+			MyStack<int> stackInt = new MyStack<int> { };
+			stackInt.Push(1);
+			stackInt.Push(2);
+			stackInt.Push(3);
+			stackInt.Push(4);
+			stackInt.Push(5);
+
+			var intArray = new int [8];
+			stackInt.CopyTo(intArray);
+			stackInt.Clear();
 		}
 	}
 }
