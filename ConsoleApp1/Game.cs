d71d3c3305e9304232a3c56549b470fbb96b48cf
Anatolii Levchenko <Anatolii.Levchenko@outlook.com>
2022-10-17 21:16:22 +0300

    classwork

---
+++ ConsoleApp1/Game.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+	public class Game
+	{
+		public Board Board { get; private set; }
+		public Game(int size = 15)
+		{
+			Board = new Board(15);
+		}
+
+		public void StartGame()
+		{
+			Board.Render();
+		}
+	}
+}
---
+++ ConsoleApp1/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+	public struct Point
+	{
+		public int X;
+		public int Y;
+
+		public Point(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+}
