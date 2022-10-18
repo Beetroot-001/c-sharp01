deded6862434dce53b8cf9d2dece7c8d7fd4a622
Andrey Ponomarenko <twinkytoe2@gmail.com>
2022-10-18 23:26:58 +0300

    Done HW properly

---
+++ ConsoleApp1/LinkedStack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ListElement<T>
+    {
+        public T Data { get; set; }
+        public ListElement<T> Next { get; set; }
+    }
+
+    public class LinkedStack<T>
+    {
+        ListElement<T> _head;        
+        public void Push(T value)
+        {
+            ListElement<T> element = new ListElement<T>();            
+            element.Data = value;            
+            element.Next = _head;            
+            _head = element;
+            
+        }
+
+        public T Pop()
+        {
+            if (_head == null) { throw new ArgumentNullException(); }
+            else
+            {
+                T temp = _head.Data;
+                _head = _head.Next;
+                return temp;
+            }
+        }
+
+        public T Peak()
+        {
+            return _head.Data;
+        }
+
+        public void Clear()
+        {
+            _head = null;
+        }
+
+        public void CopyTo(T[] values)
+        {
+            var element = _head;
+            try
+            {
+                while (element.Next != null)
+                {
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        values[i] = element.Data;
+                        element = element.Next;
+                    }
+                }
+            }
+            catch(NullReferenceException nul)
+            {
+                throw new Exception("Array outranges the stack!", nul);
+            }
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,39 +1,15 @@
 ﻿namespace ConsoleApp1
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			Stack<int> stack = new Stack<int>();
-			stack.Push(1);
-			stack.Push(2);
-			stack.Push(3);
-            Console.WriteLine(stack.Pop());
-            Console.WriteLine(stack.Pop());
-            Console.WriteLine(stack.Peek());
-            Console.WriteLine(stack.Peek());
-            Console.WriteLine(stack.Count());
-            int[] longs = null;
-            stack.CopyTo(ref longs);
-            for (int i = 0; i < longs.Length; i++)
-            {
-                Console.WriteLine(longs[i]);
-            }
-            Stack<string> stack1 = new Stack<string>();
-            stack1.Push("hello");
-            stack1.Push("world");
-            stack1.Push("!");
-            Console.WriteLine(stack1.Pop());
-            Console.WriteLine(stack1.Pop());
-            Console.WriteLine(stack1.Peek());
-            Console.WriteLine(stack1.Peek());
-            Console.WriteLine(stack1.Count());           
-            string[] strings = null;
-            stack1.CopyTo(ref strings);
-            for (int i = 0; i < strings.Length; i++)
-            {
-                Console.WriteLine(strings[i]);
-            }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedStack<int> ints = new LinkedStack<int>();
+            ints.Push(1);
+            ints.Push(2);
+            ints.Push(3);            
+            int[] ints1 = new int[3];
+            ints.CopyTo(ints1);
         }
     }
 }
