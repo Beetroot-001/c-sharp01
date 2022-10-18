7c5f3013fa3756a254bdc8c948a6cecac97187a8
Alex <Xanter22b@gmail.com>
2022-10-18 09:52:29 +0300

    start

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,65 +3,21 @@
 	internal class Program
 	{
 
-        public delegate void Message();
+       
 
-		public static void Greeting()
-		{
-			Console.WriteLine("Hello");
-		}
-		
-		public static void Greeting2()
-		{
-			Console.WriteLine("Motherfucker");
-		}
-
-		public static void Calc(int a, int b, string name)
-		{
-			int result = a + b;
-
-			Console.WriteLine($"{name}, your result is {result}");
-		}
-
-		public static bool Fair(int a)
-		{
-			if (a%2 == 0)
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-
-		public static int Calculate (int a)
-		{
-			return a * 5;
-		}
 
 
 		static void Main(string[] args)
 		{
-			Message mes;
 
-			//mes = Greeting;
-			//mes += Greeting2;
+			Board game = new Board(30);
 
-			//mes.Invoke();
+			game.Render();
 
-			//Action<int, int, string> action = Calc;
-			//Predicate<int> isFair;
-
-			//isFair = Fair;
-
-			//Console.WriteLine(isFair.Invoke(6));
+			Console.ReadKey();
 
 
-			Func<int, int> func;
 
-			func = Calculate;
-
-			Console.WriteLine(func.Invoke(4));
 		}
 	}
 }
---
+++ ConsoleApp1/Snake.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using System.Timers;
+
+namespace ConsoleApp1
+{
+    internal class Snake
+    {
+
+        private List<Point> body = new List<Point>();
+
+        public Snake()
+        {
+            body = new List<Point>
+            {
+                new Point(1,1),
+                new Point(2,1),
+                new Point(3,1),
+            };
+        }
+
+        public List<Point> Body => body;
+        public int SnakeSize => body.Count; 
+
+
+
+        public void Move(object? sender, ElapsedEventArgs e)
+        {
+            Console.SetCursorPosition(Body[0].X, Body[0].Y);
+            Console.Write(" ");
+
+            Body.RemoveAt(0);
+            Body.Add(new Point(Body.Last().X + 1, Body.Last().Y));
+
+            Console.ForegroundColor = ConsoleColor.Green;
+           
+            for (int i = 0; i < Body.Count; i++)
+            {
+                Point item = Body[i];
+                Console.SetCursorPosition(item.X, item.Y);
+                Console.Write("*");
+            }
+
+
+
+
+
+
+        }
+
+    }
+}
