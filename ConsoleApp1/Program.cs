8ec52c0e75d28ffe06fdfbc81541b0a0fe2bb261
SerhiiBas <basserhii@gmail.com>
2022-10-24 14:48:09 +0300

    sbas/homework/19-linq

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,91 +8,95 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
-			//int[] ints = new[] { 1, 23, 45, 6, 7, };
-			//var oddNumbers = ints.Where(x => x % 2 == 1).ToList();
+			sbas_homework_19_linq.Mhedod();
+                //int[] ints = new[] { 1, 23, 45, 6, 7, };
+                //var oddNumbers = ints.Where(x => x % 2 == 1).ToList();
 
-			//var t = new
-			//{
-			//	a = 1,
-			//	b = 2
-			//};
+                //var t = new
+                //{
+                //	a = 1,
+                //	b = 2
+                //};
 
-			//var squaredNumbers = ints.Select(x => new { Number = x, Square = x * x }).ToList();
-
-			//var anotherInts = new[] { 1, 1, 2, 3, 4, 5, 5 };
-			//var groupedInts = anotherInts.GroupBy(x => x).ToDictionary(x => x.Key, x => x.ToList());
+                //var squaredNumbers = ints.Select(x => new { Number = x, Square = x * x }).ToList();
 
 
-			//Console.WriteLine(string.Join(',', oddNumbers));
-			//Console.WriteLine(string.Join(',', squaredNumbers));
+                //var anotherInts = new[] { 1, 1, 2, 3, 4, 5, 5 };
+                //var groupedInts = anotherInts.GroupBy(x => x).ToDictionary(x => x.Key, x => x.ToList());
 
-			var people = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
+                //Console.WriteLine(string.Join(',', oddNumbers));
+                //Console.WriteLine(string.Join(',', squaredNumbers));
 
-			var males = people.Count(x => x.Gender == Gender.Male);
-			Console.WriteLine($"Males: {males}");
+                var people = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
 
-			var females = people.Count(x => x.Gender == Gender.Female);
-			Console.WriteLine($"Females: {females}");
+				var males = people.Count(x => x.Gender == Gender.Male);
+				Console.WriteLine($"Males: {males}");
 
-			var grouped = people.GroupBy(x => x.Gender).ToDictionary(x => x.Key, x => x.Count());
-			foreach (var item in grouped)
-			{
-				Console.WriteLine(item);
+				var females = people.Count(x => x.Gender == Gender.Female);
+				Console.WriteLine($"Females: {females}");
+
+				var grouped = people.GroupBy(x => x.Gender).ToDictionary(y => y.Key, y => y.Count());
+				foreach (var item in grouped)
+				{
+					Console.WriteLine(item);
+				}
+
+				var youngest = people.Min(x => x.Age);
+				var youngestPerson = people.MinBy(x => x.Age);
+				Console.WriteLine($"Min age: {youngest}");
+
+				var oldest = people.Max(x => x.Age);
+				var oldestPerson = people.MaxBy(x => x.Age);
+				Console.WriteLine($"Max age: {oldest}");
+
+				var averageAge = people.Average(x =>
+				{
+					return x.Age;
+				});
+				Console.WriteLine($"Average age: {averageAge}");
+
+				var nearestPerson = people.Select(x => new { Number = Math.Abs(x.Age - averageAge), person = x }).OrderBy(x => x.Number).First().person;
+				Console.WriteLine();
+
+				var isActiveCount = people.Count(x => x.IsActive);
+				Console.WriteLine($"IsActiveCount : {isActiveCount}");
+
+				CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+				var averageBalance = people.Average(x => decimal.Parse(x.Balance.Substring(1)));
+				Console.WriteLine($"Avarage balance: {averageBalance}");
+
+				var eyeColorGroups = people
+					.GroupBy(x => x.EyeColor)
+					.ToDictionary(x => x.Key, x => new { Count = x.Count() });
+
+				var peopleWithSameName = people.GroupBy(x => x.Name.Split(' ')[0])
+					.ToDictionary(x => x.Key, x => x);
+
+				var firstRegistered = people.MinBy(x => x.Registered);
+				var lastRegistered = people.MaxBy(x => x.Registered);
+
+				//[ [1,2], [3], [4]]
+				// [1,2,3,4]
+				//
+				var friends = people.Select(x => x.Friends);
+
+				var res = Enumerable.Range(1, 5).Aggregate((current, accum) => current * accum);
+
+				Console.WriteLine();
+
 			}
 
-			var youngest = people.Min(x => x.Age);
-			var youngestPerson = people.MinBy(x => x.Age);
-			Console.WriteLine($"Min age: {youngest}");
+			//Action<int> action = Method;
 
-			var oldest = people.Max(x => x.Age);
-			var oldestPerson = people.MaxBy(x => x.Age);
-			Console.WriteLine($"Max age: {oldest}");
+			//for (int i = 0; i < 10; i++)
+			//{
+			//	action(i);
+			//}
 
-			var averageAge = people.Average(x => {
-				return x.Age;
-			});
-			Console.WriteLine($"Average age: {averageAge}");
-
-			var nearestPerson = people.Select(x => new { Number = Math.Abs(x.Age - averageAge), person = x }).OrderBy(x => x.Number).First().person;
-			Console.WriteLine();
-
-			var isActiveCount = people.Count(x => x.IsActive);
-			Console.WriteLine($"IsActiveCount : {isActiveCount}");
-
-			CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
-			var averageBalance = people.Average(x => decimal.Parse(x.Balance.Substring(1)));
-			Console.WriteLine($"Avarage balance: {averageBalance}");
-
-			var eyeColorGroups = people
-				.GroupBy(x => x.EyeColor)
-				.ToDictionary(x => x.Key, x => new { Count = x.Count() });
-
-			var peopleWithSameName = people.GroupBy(x => x.Name.Split(' ')[0])
-				.ToDictionary(x => x.Key, x => x);
-
-			var firstRegistered = people.MinBy(x => x.Registered);
-			var lastRegistered = people.MaxBy(x => x.Registered);
-
-			//[ [1,2], [3], [4]]
-			// [1,2,3,4]
-			//
-			var friends = people.Select(x => x.Friends);
-
-			var res = Enumerable.Range(1, 5).Aggregate((current, accum) => current * accum);
-
-			Console.WriteLine();
+			//public static void Method(int i)
+			//{
+			//	Console.WriteLine(i);
+			//}
 		}
-
-		//Action<int> action = Method;
-
-		//for (int i = 0; i < 10; i++)
-		//{
-		//	action(i);
-		//}
-
-		//public static void Method(int i)
-		//{
-		//	Console.WriteLine(i);
-		//}
 	}
-}
+
---
+++ ConsoleApp1/sbas_homework_19_linq.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class sbas_homework_19_linq
+    {
+        /*
+           finish all tasks from lesson using LinqExample.zip and do next:
+
+        1. find out who is located farthest north/south/west/east using latitude/longitude data
+        2. find max and min distance between 2 persons
+        3. find 2 persons whos ‘about’ have the most same words
+        4. find persons with same friends (compare by friend’s name)
+
+        */
+
+        public static void Mhedod()
+        {
+            var people = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
+
+            //1
+            var farthestNorth = people.MaxBy(x=> x.Latitude);
+            var farthestSouth = people.MinBy(x=> x.Latitude);
+            var farthestWest = people.MaxBy(x=> x.Longitude);
+            var farthestEast = people.MinBy(x=> x.Longitude);
+
+            //2
+
+            var maxDistance = people.Select(x => people.Select(y => (x.Latitude - y.Latitude)).ToList()).ToList();// вкладений цикл
+            var maxDistance1 = people.Select(x => people.Select(y => (x.Longitude - y.Longitude)).ToList()).ToList();
+
+            //3
+            var twoPerson = people.GroupBy(x => x.About.Split(' '));
+
+        }
+
+    }
+}
