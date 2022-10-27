9bc6191e4dd1d0535eb5a11a98beeea243b6d48d
Tysyatsky <tysyatsky@gmail.com>
2022-10-27 16:28:48 +0300

    Update 27.10
    Can show:
    Classes
    Properties
    Methods
    Return types
    Params of methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,73 @@
-﻿namespace ConsoleApp1
+﻿using System.Reflection;
+
+namespace ConsoleApp1
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
+			var info = Assembly.GetAssembly(typeof(Program))?.GetTypes();
+
+			var classesInfo = info.Where(x => x.IsClass);
+			Console.WriteLine("Classes: ");
+			foreach (var assembly in classesInfo)
+			{
+				Console.WriteLine(assembly.Name);
+
+				Console.WriteLine("Properties: ");
+				foreach (var property in assembly.GetProperties())
+				{
+					Console.WriteLine(property.Name);
+				}
+				Console.WriteLine();
+
+				Console.WriteLine("Methods: ");
+				foreach (var method in assembly.GetMethods())
+				{
+					Console.WriteLine(method.Name);
+					var param = method.GetParameters();
+
+					Console.WriteLine("Parameters: ");
+					foreach (var parameter in param)
+					{
+						Console.WriteLine(parameter.Name);
+						Console.WriteLine(parameter.ParameterType);
+                    }
+					Console.WriteLine();
+
+					Console.WriteLine("Return type: ");
+					Console.WriteLine(method.ReturnType);
+					Console.WriteLine();
+				}
+				Console.WriteLine();
+            }
+
+			Console.WriteLine();
 		}
 	}
+
+	internal class Fruit
+	{
+		public string Name { get; set; }
+
+		public void Print()
+		{
+			Console.WriteLine(Name);
+		}
+	}
+
+	internal class Car
+	{
+		public int ID { get; set; }
+
+		public bool IsAlive { get; set; }
+
+		public int NewID(int newId)
+		{	
+			ID = newId;
+			return IsAlive ? newId : ID;
+		}
+	}
+
+
 }
