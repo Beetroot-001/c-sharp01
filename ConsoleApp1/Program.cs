b23937ec7281ac80d5b618782b5fa3a33d23521f
Tysyatsky <tysyatsky@gmail.com>
2022-10-27 14:30:19 +0300

    Update 27.10
    1. First part changed
    2. Second part is complete
    3. Third part is complete
    4. Fourth part is yet to come

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,103 +1,84 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace ConsoleApp1
 {
     class Program
-    {   
+    {
+        /// <summary>
+        /// Calculating between two points using longitude and latitude
+        /// </summary>
+        /// <param name="xLong">Longitude of first element</param>
+        /// <param name="xLat">Latitude of the first element</param>
+        /// <param name="yLong">Longitude of second element</param>
+        /// <param name="yLat">Latitude of second element</param>
+        /// <returns>Distance</returns>
+        static double Distance(double xLong, double xLat, double yLong, double yLat) => Math.Sqrt(Math.Pow(xLong - yLong, 2) + Math.Pow(xLat - yLat, 2));
+
+        /// <summary>
+        /// Calculate normalized similarity number of two strings.
+        /// </summary>
+        /// <param name="source">First string</param>
+        /// <param name="target">Second string</param>
+        /// <returns>Normalized similarity number</returns>
+        static double CalculateSimilarity(string source, string target)
+        {
+            if ((source == null) || (target == null)) return 0.0;
+            if ((source.Length == 0) || (target.Length == 0)) return 0.0;
+            if (source == target) return 1.0;
+
+            var sourceSplit = source.Split(new[] { ' ', ',', '.' });
+            var targetSplit = target.Split(new[] { ' ', ',', '.' });
+
+            int result = 0;
+
+            foreach (var word in sourceSplit)
+            {
+                if (targetSplit.Contains(word))
+                    result++;
+            }
+
+            return (1.0 - ((double)result / (double)Math.Max(source.Length, target.Length)));
+        }
+
         static void Main(string[] args)
         {
             var people = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
 
-            var personEast = people.Where(x => x.Longitude == people
-            .Select((x) => x.Longitude).Max()).Single();
-            var personNorth = people.Where(x => x.Latitude == people
-            .Select((x) => x.Latitude).Max()).Single();
-            var personWest = people.Where(x => x.Longitude == people
-            .Select((x) => x.Longitude).Min()).Single();
-            var personSouth = people.Where(x => x.Latitude == people
-            .Select((x) => x.Latitude).Min()).Single();
+            var personEast = people.MaxBy(x => x.Longitude);
+            var personNorth = people.MaxBy(x => x.Latitude);
+            var personWest = people.MinBy(x => x.Longitude);
+            var personSouth = people.MinBy(x => x.Latitude);
 
             Console.WriteLine($"{personNorth.Name} is located farthest north Position: ({personNorth.Longitude}), ({personNorth.Latitude})");
             Console.WriteLine($"{personSouth.Name} is located farthest south Position: ({personSouth.Longitude}), ({personSouth.Latitude})");
             Console.WriteLine($"{personWest.Name} is located farthest west Position: ({personWest.Longitude}), ({personWest.Latitude})");
             Console.WriteLine($"{personEast.Name} is located farthest east Position: ({personEast.Longitude}), ({personEast.Latitude})");
 
-            List<Person> personList = people.ToList();
-            int index = 0;
-            var farthest = people.Where((x) =>
-            index != personList.Count()).Select((x) =>
-            Math.Sqrt(Math.Pow(personList[index++].Longitude - x.Longitude, 2) + Math.Pow(personList[index++].Latitude - x.Latitude, 2))).Max();
-            index = 0;
-            var nearest = people.Where((x) =>
-            index != personList.Count()).Select((x) =>
-            Math.Sqrt(Math.Pow(personList[index++].Longitude - x.Longitude, 2) + Math.Pow(personList[index++].Latitude - x.Latitude, 2))).Min();
+            var distanceMax = people.Select(x => people
+                                    .Select(y => Distance(xLat: x.Latitude, yLat: y.Latitude, xLong: x.Longitude, yLong: y.Longitude))
+                                    .Max()).Max();
+            var distanceMin = people.Select(x => people
+                                    .Select(y => Distance(xLat: x.Latitude, yLat: y.Latitude, xLong: x.Longitude, yLong: y.Longitude)).OrderBy(x => x).Skip(1)
+                                    .Min()).Min();
 
-            Console.WriteLine(farthest);
-            Console.WriteLine(nearest);
+            Console.WriteLine($"Maximum distance: {distanceMax}");
+            Console.WriteLine($"Minimum distance: {distanceMin}");
 
-
-
+            var aboutsMax = people.Select(x => people
+                                  .Select(y => new { similarity = CalculateSimilarity(x.About, y.About), people = (first: x, second: y) })
+                                  .OrderByDescending(x => x.similarity).Skip(1).First())
+                                  .MaxBy(x => x.similarity);
+            
+            Console.WriteLine($"{aboutsMax.similarity} {aboutsMax.people.first.Name} {aboutsMax.people.second.Name}");
+           
             Console.WriteLine();
-
-
-            //var males = people.Count(x => x.Gender == Gender.Male);
-            //Console.WriteLine($"Males: {males}");
-
-            //var females = people.Count(x => x.Gender == Gender.Female);
-            //Console.WriteLine($"Females: {females}");
-
-            //var grouped = people.GroupBy(x => x.Gender).ToDictionary(x => x.Key, x => x.Count());
-            //foreach (var item in grouped)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-            //var youngest = people.Min(x => x.Age);
-            //var youngestPerson = people.MinBy(x => x.Age);
-            //Console.WriteLine($"Min age: {youngest}");
-
-            //var oldest = people.Max(x => x.Age);
-            //var oldestPerson = people.MaxBy(x => x.Age);
-            //Console.WriteLine($"Max age: {oldest}");
-
-            //var averageAge = people.Average(x =>
-            //{
-            //    return x.Age;
-            //});
-            //Console.WriteLine($"Average age: {averageAge}");
-
-            //var nearestPerson = people.Select(x => new { Number = Math.Abs(x.Age - averageAge), person = x }).OrderBy(x => x.Number).First().person;
-            //Console.WriteLine();
-
-            //var isActiveCount = people.Count(x => x.IsActive);
-            //Console.WriteLine($"IsActiveCount : {isActiveCount}");
-
-            //CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
-            //var averageBalance = people.Average(x => decimal.Parse(x.Balance.Substring(1)));
-            //Console.WriteLine($"Avarage balance: {averageBalance}");
-
-            //var eyeColorGroups = people
-            //    .GroupBy(x => x.EyeColor)
-            //    .ToDictionary(x => x.Key, x => new { Count = x.Count() });
-
-            //var peopleWithSameName = people.GroupBy(x => x.Name.Split(' ')[0])
-            //    .ToDictionary(x => x.Key, x => x);
-
-            //var firstRegistered = people.MinBy(x => x.Registered);
-            //var lastRegistered = people.MaxBy(x => x.Registered);
-
-            ////[ [1,2], [3], [4]]
-            //// [1,2,3,4]
-            ////
-            //var friends = people.Select(x => x.Friends);
-
-            //var res = Enumerable.Range(1, 5).Aggregate((current, accum) => current * accum);
-
-            //Console.WriteLine();
         }
 
     }
