bee95dfa260adf5b572eadd6b8db1ccf4dbe2e99
Talkontar <artempopovartempopov@gmail.com>
2022-10-28 15:01:05 +0300

    homework 16 generics

---
+++ ConsoleApp1/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Node<T>
+    {
+        public T Data;
+        public Node<T>? Prev;
+
+        public Node(T data)
+        {
+            Data = data;
+            Prev = null;
+        }
+
+        public Node(T data, Node<T>? prev) : this(data)
+        {
+            Prev = prev;    
+        }
+    }
+}
