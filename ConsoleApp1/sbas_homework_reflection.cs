a5834bd51c20dfa3806bc83192f13153bb052cc0
SerhiiBas <basserhii@gmail.com>
2022-10-28 16:24:34 +0300

    Enter path,  all prop and class

--- ConsoleApp1/sbas_homework_reflection.cs
+++ ConsoleApp1/sbas_homework_reflection.cs
@@ -12,9 +12,16 @@ namespace ConsoleApp1
     {
         public void Foo()
         {
-            string path = Path.Combine("D:\\C#\\Test_snake\\bin\\Debug\\net6.0", "Test_snake.dll"); 
-            var assembly = Assembly.LoadFrom(path);
+            Console.WriteLine("Enter Path for file!");
+            string pathforFile = Console.ReadLine() ?? throw new FormatException();
 
+            Console.WriteLine("Enter name Fail");
+            string nameFail = Console.ReadLine() ?? throw new FormatException();
+
+            string path = Path.Combine(pathforFile, nameFail);
+
+            var assembly = Assembly.LoadFrom(path)??throw new NullReferenceException();
+         
             Type typeAssembly = assembly.GetType();
 
             foreach (var item in assembly.GetTypes())
@@ -23,16 +30,20 @@ namespace ConsoleApp1
 
                 Console.WriteLine($"Name of namespace: {item.Namespace}, Name of types: {item.Name}, Base type: {item.BaseType}");
                 Console.WriteLine("__________________Property_______________\n");
-                foreach (var item1 in item.GetProperties())
+                foreach (var item1 in item.GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static))
                 {
                     Console.WriteLine($"type: {item1.GetType()}, Property name: {item1.Name},Value:  {item1.CanWrite}, Metod:  {item1.GetMethod}, type prop: {item1.PropertyType}");
                 }
 
                 Console.WriteLine("________________metods________________\n");
-                foreach (var item2 in item.GetMethods())
+                foreach (var item2 in item.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static))
                 {
                     Console.WriteLine("\n");
                     Console.WriteLine($"Metod name: {item2.Name}, Parametrs: {item2.ReturnParameter}, Return type : {item2.ReturnType}, is private: {item2.IsPrivate}");
+                    foreach (var item3 in item2.GetParameters())
+                    {
+                        Console.WriteLine($"Get parametr name: {item3.Name}, type {item3.ParameterType},hes default value: {item3.HasDefaultValue}");
+                    }
                 }
             }       
         }
