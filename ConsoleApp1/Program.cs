4abc5666cee8a1f63295dee6bf6c5d6c1f449b48
Tysyatsky <tysyatsky@gmail.com>
2022-11-02 16:47:53 +0200

    Upload
    Shiba Inu images links download.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,40 @@
-﻿namespace ConsoleApp1
+﻿using System.Collections;
+using static System.Net.Mime.MediaTypeNames;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Security.Principal;
+
+namespace ConsoleApp1
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+        static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
 		{
-		}
-	}
+            var result = GetShiba();
+
+            result.Wait();
+
+            string[] result2 = result.Result
+                .Split(new[] { '[', ',', ']', '"', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+            Load(result2);
+
+            Console.WriteLine();
+        }
+
+        static async Task<string> GetShiba(int count = 1)
+        {
+            HttpResponseMessage response = await client.GetAsync
+                ($"http://shibe.online/api/shibes?count={count}&urls=true&httpsUrls=false");
+            Console.WriteLine(response.StatusCode);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        static void Load(string[] toWrite)
+        {   
+            File.AppendAllLines("randomShibaPics.txt",toWrite);
+        }
+    }
 }
