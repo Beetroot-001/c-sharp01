e9b08681a515bb7344c36a46badad57d44ed4b91
Tysyatsky <tysyatsky@gmail.com>
2022-11-08 20:59:34 +0200

    Upload
    Basic SQL

--- ConsoleApp1/classwork.sql
+++ ConsoleApp1/classwork.sql
@@ -1,99 +1,50 @@
 ﻿USE PersonsDB
-
-
--- insert
+ insert
 InSerT INTO dbo.Persons (FirstName, LastName, Gender, Age, [Address])
-VALUES 
+VALUES
 ('Mykola', 'Test', 'M', 16, NULL),
 ('Olena', 'Pchilka', 'F', 22, 'Kyiv'),
 ('Taras', 'Shevchenko', 'M', 30, 'Kaniv'),
 ('Lina', 'Kostenko', 'F', 60, 'Lviv')
-
--- select all implicitly
-SELECT * FROM dbo.Persons
-
--- select all explicitly
-SELECT 
-	p.Id AS Testid, 
-	p.LastName AS 'Last Name', 
-	p.Age, 
-	p.FirstName, 
-	p.Gender, 
-	p.[Address]
-FROM dbo.Persons AS p
-WHERE p.Age != 30 -- > < >= <=
-
--- select all explicitly
-SELECT 
-	p.Id AS Testid, 
-	p.LastName AS 'Last Name', 
-	p.Age, 
-	p.FirstName, 
-	p.Gender, 
-	p.[Address]
-FROM dbo.Persons AS p
-WHERE p.Age IN (20, 22, 30)
-
-SELECT 
-	p.Id AS Testid, 
-	p.LastName AS 'Last Name', 
-	p.Age, 
-	p.FirstName, 
-	p.Gender, 
-	p.[Address]
-FROM dbo.Persons AS p
-WHERE p.FirstName LIKE 'a%' 
-
--- combined filters
-SELECT 
-	p.Id AS Testid, 
-	p.LastName AS 'Last Name', 
-	p.Age, 
-	p.FirstName, 
-	p.Gender, 
-	p.[Address]
-FROM dbo.Persons AS p
-WHERE p.Age IN (20, 22, 30) AND p.Gender = 'F' OR Id < 4
-
--- sorting
-SELECT 
-	p.Id AS Testid, 
-	p.LastName AS 'Last Name', 
-	p.Age, 
-	p.FirstName, 
-	p.Gender, 
-	p.[Address]
-FROM dbo.Persons AS p
-ORDER BY p.LastName ASC, p.FirstName ASC
-
--- update
+SELECT
+	Id AS Testid,
+	LastName AS 'Last Name',
+	Age,
+	FirstName,
+	Gender,
+	[Address]
+FROM dbo.Persons
+WHERE Gender = 'M'
+SELECT
+	Id AS Testid,
+	LastName AS 'Last Name',
+	Age,
+	FirstName,
+	Gender,
+	[Address]
+FROM dbo.Persons
+WHERE Age IN (16, 17, 18, 19)
+SELECT
+	Id AS Testid,
+	LastName AS 'Last Name',
+	Age,
+	FirstName,
+	Gender,
+	[Address]
+FROM dbo.Persons
+WHERE [Address] IS NULL
 UPDATE dbo.Persons
-SET FirstName = 'Natalia'
-WHERE Id = 6
-
--- delete
+SET Age = Age + 1
+SELECT * FROM dbo.Persons
 DELETE FROM dbo.Persons
-WHERE LastName = 'Ivanenko'
-
--- NULL 
-SELECT 
-	p.Id AS Testid, 
-	p.LastName AS 'Last Name', 
-	p.Age, 
-	p.FirstName, 
-	p.Gender, 
-	p.[Address]
-FROM dbo.Persons AS p
-WHERE p.Address IS NOT NULL
-
--- HAVING
-SELECT p.Gender, COUNT(p.Gender) AS 'Elements Count'
-FROM dbo.Persons AS p
-GROUP BY p.Gender
-HAVING COUNT(p.Gender) > 4
-
-SELECT AVG(p.Age) AS 'Avarage Age' FROM dbo.Persons AS p
-SELECT MAX(p.Age) AS 'Max Age' FROM dbo.Persons AS p
-SELECT Min(p.Age) AS 'Min Age' FROM dbo.Persons AS p
-SELECT SUM(p.Age) AS 'SUM Age' FROM dbo.Persons AS p
+WHERE [Address] IS NULL
+SELECT * FROM dbo.Persons
+SELECT COUNT(*)
+FROM dbo.Persons
+SELECT * FROM dbo.Persons
+SELECT
+	Age,
+	COUNT(1)
+FROM dbo.Persons
+GROUP BY Age
 
