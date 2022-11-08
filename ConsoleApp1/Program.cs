0f044f7d322eb9949429ffb4a679e05d40b3e1af
Alex <Xanter22b@gmail.com>
2022-11-08 11:51:06 +0200

    SQL done

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,55 @@
-﻿namespace ConsoleApp1
+﻿using Microsoft.VisualBasic;
+using System.Net;
+using System.Reflection;
+
+namespace ConsoleApp1
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-		}
+			
+
+   //         Select* From dbo.Persons
+
+			//Update dbo.Persons
+			//Set Address = null
+			//where Id = 6
+
+			//-- select all males
+			//Select* From dbo.Persons
+			//Where Gender = 'm'
+
+			//--select all persons with age about 18
+			//Select* From dbo.Persons
+			//Where Age > 18
+
+			//--select all persons without address
+			//Select* From dbo.Persons
+			//where Address is null
+
+			//--update age of all persons, add 1 year
+			//Update dbo.Persons
+			//Set Age = Age + 1
+
+			//--delete all rows without address
+			//Delete From dbo.Persons
+			//Where Address is null
+
+			//--count number of rows in table
+			//Select Count(Id) as 'Rows Number'
+			//From dbo.Persons
+
+			//--group persons by age and show how many persons with same age
+			//Select Count(FirstName) as 'Same Age', Age
+			//From dbo.Persons
+			//Group by Age
+			//Order by[Same Age] Desc
+
+
+
+
+
+        }
 	}
 }
