60766bf0750f79d4c2c79681a9cb73a69065bcf9
AndriiKovalenko90 <108003741+AndriiKovalenko90@users.noreply.github.com>
2022-11-09 14:27:40 +0200

    SQL1

--- ConsoleApp1/classwork.sql
+++ ConsoleApp1/classwork.sql
@@ -97,3 +97,58 @@ SELECT MAX(p.Age) AS 'Max Age' FROM dbo.Persons AS p
 SELECT Min(p.Age) AS 'Min Age' FROM dbo.Persons AS p
 SELECT SUM(p.Age) AS 'SUM Age' FROM dbo.Persons AS p
 
+
+-----------------------HOMEWORK------------------------
+--select all males
+SELECT Id AS TestId, 
+	   FirstName AS 'First Name',
+	   LastName AS 'Last Name',
+	   Age,
+	   Gender,
+	   Address 
+FROM dbo.Persons AS p
+WHERE p.Gender = 'M'
+
+
+--select all persons with age about 18
+SELECT Id AS TestId, 
+	   FirstName AS 'First Name',
+	   LastName AS 'Last Name',
+	   Age,
+	   Gender,
+	   Address 
+FROM dbo.Persons AS p
+WHERE p.Age > 18
+
+
+--select all persons without address
+SELECT Id AS TestId, 
+	   FirstName AS 'First Name',
+	   LastName AS 'Last Name',
+	   Age,
+	   Gender,
+	   Address 
+FROM dbo.Persons AS p
+WHERE p.Address IS NULL
+
+
+--update age of all persons, add 1 year
+UPDATE dbo.Persons 
+SET Age = Age + 1
+
+
+--delete all rows without address
+DELETE FROM dbo.Persons 
+WHERE Address IS NULL
+
+
+--count number of rows in table
+SELECT COUNT(1) FROM dbo.Persons 
+
+
+--group persons by age and show how many persons with same age
+SELECT Age, COUNT(p.Age)
+FROM dbo.Persons AS p
+GROUP BY p.Age
+
+
