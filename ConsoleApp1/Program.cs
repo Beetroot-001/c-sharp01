39bc2e97eaf3154c8a62bcfed44331359f4765b6
Talkontar <artempopovartempopov@gmail.com>
2022-11-10 18:02:43 +0200

    homework 17-collections

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,8 @@
 	{
 		static void Main(string[] args)
 		{
+			var voter = new ConsoleInterface();
+			voter.StartInterface();
 		}
 	}
 }
---
+++ ConsoleApp1/Vote.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Vote
+    {
+        private string _title;
+        private List<VoteOption> _options;
+        private int _votesCount;
+
+        public Vote(string title = "standart title")
+        {
+            _title = title;
+            _options = new List<VoteOption>();
+            _votesCount = 0;
+        }
+
+        public void VoteMethod(int optionNum)
+        {
+            if (optionNum < 0 || optionNum >= _options.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            _votesCount++;
+            _options[optionNum].Vote();
+            _options[optionNum].SetPercentage(_votesCount);
+        }
+
+        public List<VoteOption> Options => _options;
+
+        public override string ToString()
+        {
+            return _title;
+        }
+    }
+}
