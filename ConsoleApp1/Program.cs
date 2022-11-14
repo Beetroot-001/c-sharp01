5b4133464925d6febb5c90db187439836c2a086e
Andrey Ponomarenko <twinkytoe2@gmail.com>
2022-11-14 16:12:08 +0300

    Done API/Async HW

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace ConsoleApp1
+﻿using Newtonsoft.Json;
+
+namespace ConsoleApp1
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-		}
-	}
+    internal class Program
+    {
+        private static HttpClient client = new HttpClient();
+        private static string meowAPI = "https://meowfacts.herokuapp.com/?lang=ukr";
+        private static string geoAPI = "http://www.geoplugin.net/json.gp?ip=";
+
+        private static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            DateTime dateTime = DateTime.Now.Date;
+            var meow = await GetAPI(meowAPI);
+            var geo = await GetAPI(geoAPI);
+            GeoData geoData = JsonConvert.DeserializeObject<GeoData>(geo);
+            MeowData meowData = JsonConvert.DeserializeObject<MeowData>(meow);
+            var lat = geoData.geoplugin_latitude;
+            var @long = geoData.geoplugin_longitude;
+            string meteo = $"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={@long}&timezone={geoData.geoplugin_timezone}&daily=temperature_2m_max&daily=temperature_2m_min";
+            var getWeat = await GetAPI(meteo);
+            WeatherData wethData = JsonConvert.DeserializeObject<WeatherData>(getWeat);
+            var minTempList = wethData.daily.temperature_2m_min;
+            var maxTempList = wethData.daily.temperature_2m_max;
+            Console.WriteLine("Random cat fact:");
+            Console.WriteLine(meowData.data.Single());
+            Console.WriteLine("Temperature forecast for the next week:");
+
+            for (int i = 0; i < minTempList.Count; i++)
+            {
+                var res = (minTempList[i] + maxTempList[i]) / 2;
+                Console.WriteLine(dateTime.AddDays(i).DayOfWeek);
+                Console.WriteLine(res);
+            }
+        }
+
+        public static async Task<string> GetAPI(string apiAddress)
+        {
+            var getter = await client.GetAsync(apiAddress);
+            var result = await getter.Content.ReadAsStringAsync();
+            return result;
+        }
+    }
 }
---
+++ ConsoleApp1/WeatherData.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    public class Daily
+    {
+        public List<string> time { get; set; }
+        public List<double> temperature_2m_max { get; set; }
+        public List<double> temperature_2m_min { get; set; }
+    }
+
+    public class DailyUnits
+    {
+        public string time { get; set; }
+        public string temperature_2m_max { get; set; }
+        public string temperature_2m_min { get; set; }
+    }
+
+    public class WeatherData
+    {
+        public WeatherData()
+        {
+            Daily daily = new();
+            DailyUnits dailyUnits = new();
+        }
+
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public double generationtime_ms { get; set; }
+        public int utc_offset_seconds { get; set; }
+        public string timezone { get; set; }
+        public string timezone_abbreviation { get; set; }
+        public double elevation { get; set; }
+        public DailyUnits daily_units { get; set; }
+        public Daily daily { get; set; }
+    }
+}
