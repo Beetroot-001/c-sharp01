595a9e2c29ee03da4ad986e58fe4fb18c4e3e91d
Tysyatsky <tysyatsky@gmail.com>
2022-11-16 00:51:33 +0200

    Format
    Corrected formatting.

--- ConsoleApp1/classwork.sql
+++ ConsoleApp1/classwork.sql
@@ -1,50 +1,36 @@
 ﻿USE PersonsDB
- insert
-InSerT INTO dbo.Persons (FirstName, LastName, Gender, Age, [Address])
+
+INSERT INTO dbo.Persons (FirstName, LastName, Gender, Age, [Address])
 VALUES
 ('Mykola', 'Test', 'M', 16, NULL),
 ('Olena', 'Pchilka', 'F', 22, 'Kyiv'),
 ('Taras', 'Shevchenko', 'M', 30, 'Kaniv'),
-('Lina', 'Kostenko', 'F', 60, 'Lviv')
-SELECT
-	Id AS Testid,
-	LastName AS 'Last Name',
-	Age,
-	FirstName,
-	Gender,
-	[Address]
+('Lina', 'Kostenko', 'F', 60, 'Lviv');
+
+SELECT Id AS Testid, LastName AS 'Last Name', Age, FirstName, Gender, [Address]
 FROM dbo.Persons
-WHERE Gender = 'M'
-SELECT
-	Id AS Testid,
-	LastName AS 'Last Name',
-	Age,
-	FirstName,
-	Gender,
-	[Address]
+WHERE Gender = 'M';
+
+SELECT Id AS Testid, LastName AS 'Last Name', Age, FirstName, Gender, [Address]
 FROM dbo.Persons
-WHERE Age IN (16, 17, 18, 19)
-SELECT
-	Id AS Testid,
-	LastName AS 'Last Name',
-	Age,
-	FirstName,
-	Gender,
-	[Address]
+WHERE Age IN (16, 17, 18, 19);
+
+SELECT Id AS Testid, LastName AS 'Last Name', Age, FirstName, Gender, [Address]
 FROM dbo.Persons
-WHERE [Address] IS NULL
+WHERE [Address] IS NULL;
+
 UPDATE dbo.Persons
-SET Age = Age + 1
-SELECT * FROM dbo.Persons
+SET Age = Age + 1;
+
 DELETE FROM dbo.Persons
-WHERE [Address] IS NULL
-SELECT * FROM dbo.Persons
+WHERE [Address] IS NULL;
+
 SELECT COUNT(*)
+FROM dbo.Persons;
+
+SELECT Age, COUNT(1)
 FROM dbo.Persons
-SELECT * FROM dbo.Persons
-SELECT
-	Age,
-	COUNT(1)
-FROM dbo.Persons
-GROUP BY Age
+GROUP BY Age;
+
+SELECT * FROM dbo.Persons;
 
