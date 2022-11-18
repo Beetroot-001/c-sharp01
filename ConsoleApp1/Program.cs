ebb0371f57f2e280262a0c7b3deae38f39209f30
Tysyatsky <tysyatsky@gmail.com>
2022-11-18 15:14:41 +0200

    Update
    Creating a db with entity framework

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace ConsoleApp1
+﻿using System.Net.WebSockets;
+
+namespace ConsoleApp1
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
+			var context = new ShopContext();
 		}
 	}
 }
---
+++ ConsoleApp1/ShopContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ShopContext : DbContext 
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        public ShopContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=Shop;Trusted_Connection=True;");
+        }
+    }
+}
