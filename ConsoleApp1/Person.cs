40608163bb681e04b33749c4ac9a9f337cf43e94
Andrey Ponomarenko <twinkytoe2@gmail.com>
2022-11-22 11:31:39 +0300

    Refactoring done

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        private int Age { get; set; } = 0;
+
+        private void AgeOne()
+        {
+            Age++;
+        }
+    }
+}
