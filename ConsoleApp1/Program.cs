166e7bf68bc005fdd062ad7d6f706fb826d48346
Alex <Xanter22b@gmail.com>
2022-11-22 13:16:05 +0200

    ccc

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,22 @@
 ﻿namespace ConsoleApp1
+
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
+			//var myShopContext = new ShopContext();
 
-			var myShopContext = new ShopContext();
+			Range range = 1..3;
 
+			int[] array = { 1, 2, 3, 4, 5, 6, 7 };
 
-		}
+			int[] array2 = array[range];
+
+			foreach (var item in array2)
+			{
+				Console.WriteLine(item);
+			}
+        }
 	}
 }
--- ConsoleApp1/ShopContext.cs
+++ ConsoleApp1/ShopContext.cs
@@ -5,23 +5,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApp1
 {
     public class ShopContext: DbContext
     {
+
+
 
         public DbSet<Product> Products { get; set; }
 
 
         public ShopContext()
         {
+          
             Database.EnsureCreated();
+
         }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=localhost;Database=Library;Trusted_Connection=true;");
+            optionsBuilder.UseSqlServer("Server=localhost;Database=Shop;Trusted_Connection=true;");
             
         }
 
