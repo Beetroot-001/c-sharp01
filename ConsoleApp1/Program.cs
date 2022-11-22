2ce420616868f2761603ba7057a025f891420671
Alex <Xanter22b@gmail.com>
2022-11-22 13:16:21 +0200

    com--

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,16 +7,7 @@
 		{
 			//var myShopContext = new ShopContext();
 
-			Range range = 1..3;
-
-			int[] array = { 1, 2, 3, 4, 5, 6, 7 };
-
-			int[] array2 = array[range];
-
-			foreach (var item in array2)
-			{
-				Console.WriteLine(item);
-			}
+			
         }
 	}
 }
