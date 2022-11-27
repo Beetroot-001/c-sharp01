1cb4ed1291a21b1dd19b5bbff1f0b093cb6c1ea8
Andrey Ponomarenko <twinkytoe2@gmail.com>
2022-11-27 16:27:55 +0300

    Done basic unit test

--- ConsoleApp1/AssemblyService.cs
+++ ConsoleApp1/AssemblyService.cs
@@ -2,30 +2,31 @@
 
 namespace ConsoleApp1
 {
-    internal class AssemblyService
+    public class AssemblyService
     {
         private Type[] types;
-        public static IEnumerable<Type> classes;
+        private static IEnumerable<Type> classes;
+        public Assembly assembly1 { get; set; }
 
         public AssemblyService(string path)
-        {            
-            var assembly = Assembly.Load(path);
-            types = assembly.GetTypes();
-            classes = types.Where(x => x.IsClass || x.IsValueType);
+        {
+            assembly1 = Assembly.Load(path);
         }
 
-        public void GetClassesInfo()
+        public void GetClassesInfo(Assembly? assembly)
         {
-            var classInfo = AssemblyService.classes;
-            foreach (Type? assembly in classInfo)
+            types = assembly.GetTypes();
+            classes = types.Where(x => x.IsClass || x.IsValueType);
+            var classInfo = classes;
+            foreach (Type? objType in classInfo)
             {
-                if (assembly.IsClass)
-                    Console.WriteLine(" Class " + assembly.Name);
-                else if (assembly.IsValueType && !assembly.IsEnum)
-                    Console.WriteLine(" Struct " + assembly.Name);
-                else Console.WriteLine(" Enum " + assembly.Name);
+                if (objType.IsClass)
+                    Console.WriteLine(" Class " + objType.Name);
+                else if (objType.IsValueType && !objType.IsEnum)
+                    Console.WriteLine(" Struct " + objType.Name);
+                else Console.WriteLine(" Enum " + objType.Name);
                 Console.WriteLine("  Props: ");
-                var props = assembly.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+                var props = objType.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
                 if (props != null && props.Any())
                 {
                     foreach (PropertyInfo property in props)
@@ -35,7 +36,7 @@ namespace ConsoleApp1
                 }
                 else Console.WriteLine("   None");
                 Console.WriteLine("  Methods: ");
-                var methods = assembly.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
+                var methods = objType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
                 if (methods != null && methods.Any())
                 {
                     foreach (MethodInfo method in methods)
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,11 @@
-﻿using System.Drawing;
-using System.Reflection;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
             AssemblyService assemblyService = new("ConsoleApp1");
-            assemblyService.GetClassesInfo();
-        }        
+            assemblyService.GetClassesInfo(assemblyService.assembly1);
+        }
     }
 }
