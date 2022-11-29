ead3ebd6d4891bb87f9de7111a151663e809b49e
SerhiiBas <basserhii@gmail.com>
2022-11-29 02:55:55 +0200

    sbas/homework/29-html

---
+++ sbas_homework_29_html/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sbas_homework_29_html
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
